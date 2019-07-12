6157fd6e3ff7fa8fc39412bbef8d1e53153891e6
Anton Bystryakov <antonbystryakov@MacBook-Pro-Anton.local>
2019-07-12 16:14:20 +0400

    added latest commits from fetch

--- js/main.js
+++ js/main.js
@@ -1,34 +1,33 @@
 const axios = require('axios');
-
 import "babel-polyfill";
 
-const url = 'http://www.json-generator.com/api/json/get/cqclnKfhsO?indent=2';
+const widgetsUrl = 'http://www.json-generator.com/api/json/get/cqclnKfhsO?indent=2';
+const latestCommitsUrl = 'https://next.json-generator.com/api/json/get/EyfChEW-v';
 
 const controleWidgets = async () => {
-    loader();
+    printWidgetLoader();
     const {
         budget,
         operations,
         requests,
         progress
-    } = await getWidgetsData();
+    } = await fetchData(widgetsUrl);
 
     printBudgetData(budget);
     printOperationsData(operations);
     printRequestsData(requests);
     printProgressData(progress);
-    loader();
+    printWidgetLoader();
 }
 
-const loader = () => {
-    const widget = document.querySelectorAll('.small-widgets__item');
-    widget.forEach((item) => {
+const printWidgetLoader = () => {
+    const widgets = document.querySelectorAll('.small-widgets__item');
+    widgets.forEach((item) => {
         item.classList.toggle('small-widgets__item--loading');
     })
-    // console.log(widget);
 }
 
-const getWidgetsData = async () => {
+const fetchData = async url => {
     try {
         const {
             data
@@ -71,6 +70,31 @@ const printPercents = (elementId, percents) => {
 const prepareBigNumbers = value => value.toString().match(/(\d+?)(?=(\d{3})+(?!\d)|$)/g);
 
 const setHtmlIntoElement =
-    (elementId, html) => document.getElementById(elementId).innerHTML = html;
+  (elementId, html) => document.getElementById(elementId).innerHTML = html;
 
 controleWidgets()
+
+const controlLatestCommits = async () => {
+  printCommitsListLoader();
+  const data = await fetchData(latestCommitsUrl);
+  printCommitsList(data);
+}
+
+const printCommitsList = data => {
+  const html = data.map(({ message, time, link }) => `
+    <ul class="large-widgets__latest-commits__table__row">
+      <li class="large-widgets__latest-commits__table__cell">${message}</li>
+      <li class="large-widgets__latest-commits__table__cell--dark">${time}</li>
+      <li class="large-widgets__latest-commits__table__cell icon--arrow--right"><a href=${link}><i class="right-arrow"></i></a></li>
+    </ul>
+  `).join('');
+  setHtmlIntoElement('latest-commits-table', html);
+}
+
+const printCommitsListLoader = () => {
+  const rows = document.querySelectorAll('.large-widgets__latest-commits__table__row');
+  rows.forEach((item) => {
+    item.classList.toggle('large-widgets__latest-commits__table__row--loading');
+  })
+}
+controlLatestCommits();
