1d02d7917ea64205b2df33bca9a58d7fb062a210
PSimple <tpsimple@gmail.com>
2019-07-15 16:46:06 +0400

    hover transition

--- js/main.js
+++ js/main.js
@@ -6,27 +6,3 @@ export * from './popup';
 
 fillingCommitsTable();
 fillingWidgets();
-
-const widget = (states) => {
-  document.querySelectorAll('.small-widgets__item').forEach((e) => {
-    const eWidgetState = !e.classList.contains('small-widgets__item--hidden');
-    if (states[e.getAttribute('data-name').toLowerCase()] !== eWidgetState) {
-      e.classList.toggle('small-widgets__item--hidden');
-    }
-  });
-};
-
-const widgetCheckStates = {
-  budget: true,
-  operations: true,
-  requests: true,
-  progress: true,
-};
-
-document.querySelectorAll('.small-widgets__checkbox').forEach((e) => {
-  e.addEventListener('click', (element) => {
-    const elementName = element.target.name;
-    widgetCheckStates[elementName] = !widgetCheckStates[elementName];
-    widget(widgetCheckStates);
-  });
-});
--- js/widgets.js
+++ js/widgets.js
@@ -1,4 +1,7 @@
-import { setHtmlIntoElement, fetchData } from './serviceFunctions';
+import {
+  setHtmlIntoElement,
+  fetchData
+} from './serviceFunctions';
 
 const widgetsUrl = 'http://www.json-generator.com/api/json/get/cqclnKfhsO?indent=2';
 
@@ -54,5 +57,29 @@ const fillingWidgets = async () => {
   printProgressData(progress);
   printWidgetLoader();
 };
+
+const widget = (states) => {
+  document.querySelectorAll('.small-widgets__item').forEach((e) => {
+    const eWidgetState = !e.classList.contains('small-widgets__item--hidden');
+    if (states[e.getAttribute('data-name').toLowerCase()] !== eWidgetState) {
+      e.classList.toggle('small-widgets__item--hidden');
+    }
+  });
+};
+
+const widgetCheckStates = {
+  budget: true,
+  operations: true,
+  requests: true,
+  progress: true,
+};
+
+document.querySelectorAll('.small-widgets__checkbox').forEach((e) => {
+  e.addEventListener('click', (element) => {
+    const elementName = element.target.name;
+    widgetCheckStates[elementName] = !widgetCheckStates[elementName];
+    widget(widgetCheckStates);
+  });
+});
 
 export default fillingWidgets;
