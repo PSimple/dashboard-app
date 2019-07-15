73a9d0142a8ccc242a0036815cf8b125f2d74906
PSimple <tpsimple@gmail.com>
2019-07-15 15:36:49 +0400

    popup module done

--- js/main.js
+++ js/main.js
@@ -1,43 +1,11 @@
 import 'babel-polyfill';
 import fillingWidgets from './widgets';
 import fillingCommitsTable from './latestCommits';
+import * as popup from './popup';
 
 fillingCommitsTable();
 fillingWidgets();
-
-const isHidden = (el) => {
-  const style = window.getComputedStyle(el);
-  return (style.display === 'none');
-};
-
-const overlay = document.querySelector('.popup__overlay');
-
-const togglePopup = (element, visibleClass) => {
-  if ((overlay.style.display === 'none' || overlay.style.display === '') && isHidden(document.querySelector(element))) {
-    overlay.style.display = 'block';
-    overlay.addEventListener('click', () => {
-      togglePopup(element, visibleClass);
-    });
-    document.querySelector(element).classList.add(visibleClass);
-  } else {
-    overlay.style.display = 'none';
-    document.querySelector(element).classList.remove(visibleClass);
-    overlay.removeEventListener('click', () => {});
-  }
-};
-
-const buttonGrid = document.querySelector('.header__button--grid');
-const buttonProfile = document.querySelector('.header__profile');
-
-buttonGrid.addEventListener('click', () => {
-  buttonGrid.classList.toggle('header__button--active');
-  togglePopup('.popup__grid', 'popup--visible');
-});
-
-buttonProfile.addEventListener('click', () => {
-  buttonProfile.classList.toggle('header__button--active');
-  togglePopup('.popup__profile', 'popup--visible');
-});
+popup();
 
 const widget = (states) => {
   document.querySelectorAll('.small-widgets__item').forEach((e) => {
@@ -49,13 +17,16 @@ const widget = (states) => {
 };
 
 const widgetCheckStates = {
-  budget: true, operations: true, requests: true, progress: true,
+  budget: true,
+  operations: true,
+  requests: true,
+  progress: true,
 };
 
 document.querySelectorAll('.small-widgets__checkbox').forEach((e) => {
-  e.addEventListener('click', (el) => {
-    const elName = el.target.name;
-    widgetCheckStates[elName] = !widgetCheckStates[elName];
+  e.addEventListener('click', (element) => {
+    const elementName = element.target.name;
+    widgetCheckStates[elementName] = !widgetCheckStates[elementName];
     widget(widgetCheckStates);
   });
 });
