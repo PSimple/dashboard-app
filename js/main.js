016caca31cb9c2e2d03981d561a206ffac360d79
PSimple <tpsimple@gmail.com>
2019-07-15 15:28:17 +0400

    module latestCommits

--- js/main.js
+++ js/main.js
@@ -1,41 +1,8 @@
 import 'babel-polyfill';
-import { setHtmlIntoElement, fetchData } from './serviceFunctions';
 import fillingWidgets from './widgets';
+import fillingCommitsTable from './latestCommits';
 
-const latestCommitsUrl = 'https://next.json-generator.com/api/json/get/EyfChEW-v';
-
-const printCommitsList = (data) => {
-  const html = data.reduce((acc, {
-    message,
-    time,
-    link,
-  }) => `
-    ${acc}
-    <ul class="commits-table__row">
-      <li class="commits-table__cell commits-table__cell--message">${message}</li>
-      <li class="commits-table__cell commits-table__cell--time">${time}</li>
-      <li class="commits-table__cell commits-table__cell--icon-arrow-right">
-        <a class="cell-link" href=${link}><i class="right-arrow"></i></a>
-      </li>
-    </ul>
-  `, '');
-  setHtmlIntoElement('latest-commits-table', html);
-};
-
-const printCommitsListLoader = () => {
-  const rows = document.querySelectorAll('.commits-table__row');
-  rows.forEach((item) => {
-    item.classList.toggle('commits-table__row--loading');
-  });
-};
-
-const controlLatestCommits = async () => {
-  printCommitsListLoader();
-  const data = await fetchData(latestCommitsUrl);
-  printCommitsList(data);
-};
-
-controlLatestCommits();
+fillingCommitsTable();
 fillingWidgets();
 
 const isHidden = (el) => {
