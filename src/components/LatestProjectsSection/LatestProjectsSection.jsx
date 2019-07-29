7fc6dc1578ff66fbc0f15c135191700b545d99e2
Pavel.Tkachenko <pavel.tkachenko@rt.ru>
2019-07-29 13:53:02 +0400

    fix button disabled

--- src/components/LatestProjectsSection/LatestProjectsSection.jsx
+++ src/components/LatestProjectsSection/LatestProjectsSection.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import cn from 'classnames';
 import styles from './LatestProjectsSection.css';
 import BlockHeader from '../BlockHeader';
 import IconButton from '../IconButton';
@@ -16,7 +15,7 @@ const LatestProjectsSection = () => (
     <div className={styles.nav}>
       <div className={styles.pages}>1 IN 15 PAGES</div>
       <div className={styles.panel}>
-        <button className={cn(styles.button, styles.disabled)}>PREVIOUS ENTRIES</button>
+        <button className={styles.button} disabled>PREVIOUS ENTRIES</button>
         <button className={styles.button}>NEXT ENTRIES</button>
       </div>
     </div>
