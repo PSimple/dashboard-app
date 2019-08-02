b4d9eb2e2088b6c8bfddbbc8d5abaf551deb31a7
Pavel.Tkachenko <pavel.tkachenko@rt.ru>
2019-08-02 13:46:01 +0400

    fix & refactoring

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -3,7 +3,7 @@ import { Form, Field } from 'react-final-form';
 import '@babel/plugin-transform-runtime';
 import Select, { Option } from 'rc-select';
 import PropTypes from 'prop-types';
-import './Register.css';
+import styles from './Register.css';
 import 'rc-select/assets/index.css';
 
 const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
@@ -15,7 +15,7 @@ const onSubmit = async (values) => {
 
 const usernameAvailable = async (value) => {
   await sleep(400);
-  if (~['mike', 'pavel', 'kostya', 'maksim', 'anton'].indexOf(value && value.toLowerCase())) {
+  if (['mike', 'pavel', 'kostya', 'maksim', 'anton'].includes(value && value.toLowerCase())) {
     return 'Username taken!';
   }
   return null;
@@ -72,31 +72,34 @@ const Register = () => (
       render={({
         handleSubmit, values,
       }) => (
-        <form onSubmit={handleSubmit}>
-          <Field name="username" validate={usernameAvailable}>
+        <form
+          onSubmit={handleSubmit}
+          className={styles.register}
+        >
+          <Field name="username"validate={usernameAvailable}>
             {({ input, meta }) => (
-              <div>
-                <label>Username</label>
-                <input {...input} type="text" placeholder="Username" />
-                {meta.error && meta.touched && <span>{meta.error}</span>}
+              <div className={styles.row}>
+                <label className={styles.label}>Username</label>
+                <input {...input} className={styles.input} type="text" placeholder="Username" />
+                {meta.error && meta.touched && <span className={styles.error}>{meta.error}</span>}
               </div>
             )}
           </Field>
           <Field name="password">
             {({ input, meta }) => (
-              <div>
-                <label>Password</label>
-                <input {...input} type="password" placeholder="Password" />
-                {meta.error && meta.touched && <span>{meta.error}</span>}
+              <div className={styles.row}>
+                <label className={styles.label}>Password</label>
+                <input {...input} className={styles.input} type="password" placeholder="Password" />
+                {meta.error && meta.touched && <span className={styles.error}>{meta.error}</span>}
               </div>
             )}
           </Field>
           <Field name="confirm">
             {({ input, meta }) => (
-              <div>
-                <label>Confirm</label>
-                <input {...input} type="password" placeholder="Confirm" />
-                {meta.error && meta.touched && <span>{meta.error}</span>}
+              <div className={styles.row}>
+                <label className={styles.label}>Confirm</label>
+                <input {...input} className={styles.input} type="password" placeholder="Confirm" />
+                {meta.error && meta.touched && <span className={styles.error}>{meta.error}</span>}
               </div>
             )}
           </Field>
@@ -104,52 +107,55 @@ const Register = () => (
             initialValue={levelsList[0] && levelsList[0].value}
           >
             {({ input, meta }) => (
-              <div>
-                <label>Select</label>
+              <div className={styles.row}>
+                <label className={styles.label}>Select</label>
                 <Select
                   style={{ width: 365 }}
-                  {...input}>
+                  {...input}
+                  className={styles['rc-select']}>
                   {levelsList.map(option => (
                     <Option key={option.value} value={option.value}>{option.value}</Option>
                   ))
                   }
                 </Select>
-                {meta.error && meta.touched && <span>{meta.error}</span>}
+                {meta.error && meta.touched && <span className={styles.error}>{meta.error}</span>}
               </div>
             )}
           </Field>
-          <div>
-            <label>Repository</label>
+          <div className={styles.row}>
+            <label className={styles.label}>Repository</label>
             {repositoryList.map(option => (
-              <label key={option.value}>
-                <Field
-                  name='repository'
-                  type='radio'
-                  value={option.value}
-                  initialValue={option.value === 'github' ? option.value : undefined}
-                >
-                  {({ input, meta }) => (
-                    <div>
-                      <input {...input}/>
-                      {meta.error && meta.touched && <span>{meta.error}</span>}
-                    </div>
-                  )}
-                </Field>
-                {` ${option.value}`}
-              </label>
+                <div key={option.value} className={styles.radio}>
+                  <Field
+                    name='repository'
+                    type='radio'
+                    value={option.value}
+                    initialValue='github'
+                  >
+                    {({ input, meta }) => (
+                      <>
+                        <input {...input} className={styles.input}/>
+                        {meta.error && meta.touched
+                          && <span className={styles.error}>{meta.error}</span>
+                          }
+                      </>
+                    )}
+                  </Field>
+                  {` ${option.value}`}
+                </div>
             ))}
           </div>
           <Field name="check" type='checkbox' initialValue={false}>
             {({ input, meta }) => (
-              <div>
-                <label>Check</label>
-                <input {...input} />
+              <div className={styles.row}>
+                <label className={styles.label}>Check</label>
+                <input {...input} className={styles.input} />
                 {meta.error && meta.touched && <span>{meta.error}</span>}
               </div>
             )}
           </Field>
-          <div className="buttons">
-            <button type="submit" disabled={!values.check}>
+          <div className={styles.buttons}>
+            <button type="submit" className={styles.button} disabled={!values.check}>
               Submit
             </button>
           </div>
